f966abf2401e55a3b6bcc3f4b69e36758093ef0f
julia eka <eka47577@gmail.com>
2022-02-05 18:57:03 +0700

    rest full

--- application/controllers/api/Apibarang.php
+++ application/controllers/api/Apibarang.php
@@ -18,13 +18,92 @@ class Apibarang extends REST_Controller
     //Menampilkan data barang
     function index_get()
     {
-        $barang = $this->Apibarang_model->getbarang();
+        $id = $this->get('id');
+        if ($id == null) {
+            $barang = $this->Apibarang_model->getbarang();
+        }else{
+            $barang = $this->Apibarang_model->getbarang($id);
+        }
         
         if ($barang) {
-            $this->response($barang, REST_Controller::HTTP_OK);
+            $this->response([
+                'status' => true,
+                'data' => $barang,
+            ], REST_Controller::HTTP_OK);
+        }else{
+            $this->response([
+                'status' => false,
+                'message' => 'data tidak ditemukan!',
+            ], REST_Controller::HTTP_NOT_FOUND);
+        }
+    }
+    public function index_delete(){
+        $id = $this->delete('id');
+
+        if ($id == null) {
+            $this->response([
+                'status' => false,
+                'message' => 'anda tidak mengirim id',
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }else{
+            if ($this->Apibarang_model->hapusbarang($id) > 0 ) {
+                $this->response([
+                    'status' => true,
+                        'id' => $id,
+                        'message' => 'berhasil menghapus',
+                ], REST_Controller::HTTP_OK);
+            }else{
+                $this->response([
+                    'status' => false,
+                    'message' => 'gagal hapus data!',
+                ], REST_Controller::HTTP_BAD_REQUEST);
+            }
         }
     }
 
+    public function index_post(){
+        $data = [
+            'id_brg' => $this->post('id_brg'),
+            'nama_brg' => $this->post('nama_brg'),
+            'keterangan' => $this->post('keterangan'),
+            'kategori' => $this->post('kategori'),
+            'harga' => $this->post('harga'),
+            'stok' => $this->post('stok')
+        ];
+        if ($this->Apibarang_model->tambahbarang($data) > 0) {
+            $this->response([
+                'status' => true,
+                    'message' => 'berhasil tambah data',
+            ], REST_Controller::HTTP_CREATED);
+        }else{
+            $this->response([
+                'status' => false,
+                'message' => 'gagal tambah data',
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+    public function index_put(){
+        $id = $this->put('id');
+        $data = [
+            'id_brg' => $this->put('id_brg'),
+            'nama_brg' => $this->put('nama_brg'),
+            'keterangan' => $this->put('keterangan'),
+            'kategori' => $this->put('kategori'),
+            'harga' => $this->put('harga'),
+            'stok' => $this->put('stok')
+        ];
+        if ($this->Apibarang_model->editbarang($data, $id) > 0) {
+            $this->response([
+                'status' => true,
+                    'message' => 'berhasil ubah data',
+            ], REST_Controller::HTTP_OK);
+        }else{
+            $this->response([
+                'status' => false,
+                'message' => 'id salah!',
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
 
     //Masukan function selanjutnya disini
 }
--- application/models/Apibarang_model.php
+++ application/models/Apibarang_model.php
@@ -2,7 +2,24 @@
 
 class Apibarang_model extends CI_Model
 {
-    public function getbarang(){
-        return $this->db->get("tb_barang")->result_array();
+    public function getbarang($id = null){
+        if ($id == null) {
+            return $this->db->get("tb_barang")->result_array();
+        }else{
+            return $this->db->get_where("tb_barang",["id_brg" => $id])->result_array();
+        }
+        
+    }
+    public function hapusbarang($id){
+        $this->db->delete('tb_barang',['id_brg'=>$id]);
+        return $this->db->affected_rows();
+    }
+    public function tambahbarang($data){
+        $this->db->insert('tb_barang', $data);
+        return $this->db->affected_rows();
+    }
+    public function editbarang($data, $id){
+        $this->db->update('tb_barang', $data, ['id_brg'=>$id]);
+        return $this->db->affected_rows();
     }
 }
